a1efe7dd3f737ea0268f04de896ec9f26950c248
xkurozaru <kurozaru.210@gmail.com>
2025-08-14 06:01:40 +0900

    Avoid warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import { Toaster } from "@/components/ui/toaster";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <Toaster />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>X List Member Extractor</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🐦</text></svg>"
+        ></link>
+        <meta
+          name="description"
+          content="Xリストのメンバーを簡単に抽出するツールです。APIとスクレイピングの2つの方法でデータを取得できます。"
+        />
+      </Head>
+      <ChakraProvider value={defaultSystem}>
+        <Toaster />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,17 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head>
-        <title>X List Member Extractor</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🐦</text></svg>"
-        ></link>
-        <meta
-          name="description"
-          content="Xリストのメンバーを簡単に抽出するツールです。APIとスクレイピングの2つの方法でデータを取得できます。"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
